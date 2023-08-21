5c553decdf0ab4d8651682a140b8c019ff6e9841
Mutaga Kihahu <kihahumutagah@gmail.com>
2023-08-21 09:58:37 +0300

    Milestone: Added a db.json file and a details component ts file

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,9 +5,9 @@ import { HousingLocation } from './housinglocation';
   providedIn: 'root'
 })
 export class HousingService {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
-  housingLocationList: HousingLocation[] = [
+  // readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  url = 'http://localhost:3000/locations';
+  /* housingLocationList: HousingLocation[] = [
     {
       id: 0,
       name: 'Acme Fresh Start Housing',
@@ -108,14 +108,20 @@ export class HousingService {
       wifi: true,
       laundry: true
     }
-  ];
+  ];*/
 
-  getAllHousingLocations(): HousingLocation[]{
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`)
+    return await data.json() ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string){
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
   constructor() { }
 }
